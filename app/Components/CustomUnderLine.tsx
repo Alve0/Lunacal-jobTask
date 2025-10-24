231263d6a6e78cc43e77981b32480187df9b81eb
alve <gamingalve@gmail.com>
2025-10-24 16:59:42 +0600

    added underline components

---
+++ app/Components/CustomUnderLine.tsx
@@ -0,0 +1,5 @@
+export default function CustomUnderLine() {
+  return (
+    <div className="mt-2 mb-2 w-full max-w-[500px] h-[6px] shadow shadow-gray-900  bg-[#ffffff10] rounded-full" />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Instruction from "./Components/Instruction";
 import Info from "./Components/Task/Info";
+import CustomUnderLine from "./Components/CustomUnderLine";
 
 export default function Home() {
   return (
     <div className="max-w-[1728px] min-h-[895px] h-screen mx-auto bg-linear-to-b from-[#373E44] to-[#191B1F] flex items-center justify-center gap-10">
       <Instruction />
-      <Info />
+      <div className="flex flex-col items-center justify-center">
+        <Info />
+        <CustomUnderLine />
+      </div>
     </div>
   );
 }
